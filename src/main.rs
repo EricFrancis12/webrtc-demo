5fe4e1b7ed2afb233271c0f5f09950940c997bb4
Eric <ericfrancis1@proton.me>
2025-12-16 22:11:33 -0600

    WIP refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,72 @@
-use std::sync::Arc;
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    ops::Range,
+    sync::{Arc, Mutex},
+};
 
 use axum::{
     Router,
-    extract::ws::{self, Utf8Bytes, WebSocket, WebSocketUpgrade},
+    extract::{
+        self, ConnectInfo, Query, WebSocketUpgrade,
+        ws::{self, Utf8Bytes, WebSocket},
+    },
     response::{Html, IntoResponse},
     routing::get,
 };
-use futures::{SinkExt, StreamExt};
+use futures::{SinkExt, StreamExt, stream::SplitSink};
 use serde::{Deserialize, Serialize};
-use tokio::sync::{
-    Mutex,
-    mpsc::{self, UnboundedSender},
-};
+use uuid::Uuid;
 
-type PeerTx = UnboundedSender<Option<UnboundedSender<ws::Message>>>;
+#[derive(Debug)]
+struct Client {
+    id: String,
+    addr: SocketAddr,
+    comm: Arc<Mutex<SplitSink<WebSocket, ws::Message>>>,
+}
+
+#[derive(Debug)]
+struct Room {
+    max_size: usize,
+    // password: Option<String>, // TODO: ...
+    host: Client,
+    guests: HashMap<Uuid, Client>,
+}
 
 #[derive(Clone)]
 struct State {
-    waiting_client: Arc<Mutex<Option<(UnboundedSender<ws::Message>, PeerTx)>>>,
+    clients: Arc<Mutex<HashMap<String, Client>>>,
+    rooms: Arc<Mutex<HashMap<u64, Room>>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "kebab-case")]
+#[serde(rename_all = "kebab-case")]
 enum MessageFromClient {
-    JoinRequest,
-    Offer { offer: serde_json::Value },
-    Answer { answer: serde_json::Value },
-    IceCandidate { candidate: serde_json::Value },
+    GetRooms,
+    CreateRoom,
+    JoinRoom { room_id: u64 },
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "kebab-case")]
+#[serde(rename_all = "kebab-case")]
 enum MessageFromServer {
-    WaitingForPartner,
-    PartnersReady { initiator: bool },
-    ServerClosing,
+    Ok,
+    BadRequest,
+    Disconnect(DisconnectReason),
+}
+
+impl MessageFromServer {
+    fn ws_msg(&self) -> ws::Message {
+        let text: Utf8Bytes = serde_json::to_string(self).unwrap().into();
+        ws::Message::Text(text)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+enum DisconnectReason {
+    InvalidClientId,
+    ClientIdTaken,
 }
 
 const PORT: u16 = 3000;
@@ -42,169 +74,147 @@ const PORT: u16 = 3000;
 #[tokio::main]
 async fn main() {
     let state = State {
-        waiting_client: Arc::new(Mutex::new(None)),
+        clients: Arc::new(Mutex::new(HashMap::new())),
+        rooms: Arc::new(Mutex::new(HashMap::new())),
     };
 
     let app = Router::new()
         .route("/", get(handle_serve_html))
         .route("/index.html", get(handle_serve_html))
-        .route("/ws", get(ws_handler))
+        .route("/ws", get(handle_ws))
         .with_state(state);
 
     println!("Server running on port {PORT}");
     let addr = format!("0.0.0.0:{PORT}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
 
 async fn handle_serve_html() -> Html<&'static str> {
     Html(include_str!("../index.html"))
 }
 
-async fn ws_handler(
+#[derive(Deserialize)]
+struct WsQuery {
+    client_id: String,
+}
+
+async fn handle_ws(
     ws: WebSocketUpgrade,
-    axum::extract::State(state): axum::extract::State<State>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    Query(query): Query<WsQuery>,
+    extract::State(state): extract::State<State>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| handle_socket(socket, state))
-}
+    println!("HERE");
 
-async fn handle_socket(socket: WebSocket, state: State) {
-    println!("New client connected");
+    ws.on_upgrade(async move |socket| {
+        let client_id = query.client_id;
+        println!("New ws connection from client_id {client_id}");
 
-    let (mut socket_sender, mut socket_receiver) = socket.split();
-    let (ws_msg_sender, mut ws_msg_receiver) = mpsc::unbounded_channel::<ws::Message>();
-    let (peer_tx_sender, mut peer_tx_receiver) =
-        mpsc::unbounded_channel::<Option<UnboundedSender<ws::Message>>>();
+        let (mut to_client, mut from_client) = socket.split();
 
-    // Spawn task to send messages from channel to client
-    let mut send_task = tokio::spawn(async move {
-        while let Some(msg) = ws_msg_receiver.recv().await {
-            if let Err(err) = socket_sender.send(msg).await {
-                println!("Error sending message: {err}");
-                break;
-            }
+        if client_id.is_empty() {
+            println!("Invalid client_id: {client_id}");
+            _ = to_client.send(MessageFromServer::Disconnect(
+                DisconnectReason::InvalidClientId,
+            ).ws_msg());
+            _ = to_client.close();
+            return;
         }
-    });
 
-    let ws_msg_sender_clone = ws_msg_sender.clone();
-    let state_clone = state.clone();
-    let peer_tx = Arc::new(Mutex::new(None::<UnboundedSender<ws::Message>>));
-    let peer_tx_clone = peer_tx.clone();
-
-    // Listen for peer assignment
-    tokio::spawn(async move {
-        if let Some(peer) = peer_tx_receiver.recv().await {
-            *peer_tx_clone.lock().await = peer;
+        let mut clients = state.clients.lock().unwrap();
+        if clients.contains_key(&client_id) {
+            println!("This client_id {client_id} is already taken");
+            _ = to_client.send(MessageFromServer::Disconnect(
+                DisconnectReason::InvalidClientId,
+            ).ws_msg());
+            _ = to_client.close();
+            return;
         }
-    });
 
-    let peer_tx_for_recv = peer_tx.clone();
-    // Receive messages from client
-    let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(msg)) = socket_receiver.next().await {
-            if let ws::Message::Close(_) = msg {
-                break;
-            }
+        let client_id = client_id.clone();
+        let state = state.clone();
 
-            let ws::Message::Text(text) = msg else {
-                println!("[Should not happen] Unhandled message from client: {msg:?}");
-                continue;
-            };
+        let to_client = Arc::new(Mutex::new(to_client));
+        let to_client_clone = to_client.clone();
+        clients.insert(client_id.clone(), Client{id: client_id.clone(), addr, comm: to_client_clone});
 
-            let Ok(data) = serde_json::from_str::<MessageFromClient>(&text) else {
-                continue;
-            };
-
-            match data {
-                MessageFromClient::JoinRequest => {
-                    handle_join_request(
-                        ws_msg_sender_clone.clone(),
-                        peer_tx_sender.clone(),
-                        &state_clone,
-                    )
-                    .await;
-                }
-                MessageFromClient::Offer { .. }
-                | MessageFromClient::Answer { .. }
-                | MessageFromClient::IceCandidate { .. } => {
-                    // Forward messages to the other peer (partner)
-                    let peer_opt = peer_tx_for_recv.lock().await;
-                    if let Some(ref peer) = *peer_opt {
-                        _ = peer.send(ws::Message::Text(text));
-                    }
-                }
-            }
-        }
-        peer_tx_for_recv.lock().await.clone()
-    });
-
-    // Wait for either task to complete/fail.
-    // Whichever finishes first causes the other to be aborted.
-    tokio::select! {
-       _= &mut recv_task => {
-            send_task.abort();
-        }
-        _ = &mut send_task => {
-            recv_task.abort();
-        }
-    }
-
-    println!("Client disconnected");
-}
-
-async fn handle_join_request(
-    ws_msg_sender: UnboundedSender<ws::Message>,
-    peer_tx_sender: PeerTx,
-    state: &State,
-) {
-    let mut waiting_client = state.waiting_client.lock().await;
-
-    if waiting_client.is_none() {
-        // First client - wait for another
-        println!("First client waiting for peer...");
-        let msg = serde_json::to_string(&MessageFromServer::WaitingForPartner)
-            .unwrap()
-            .into();
-        _ = ws_msg_sender.send(ws::Message::Text(msg));
-        *waiting_client = Some((ws_msg_sender.clone(), peer_tx_sender));
-    } else {
-        // Second client - pair them up
-        let (peer1_tx, peer1_peer_sender) = waiting_client.take().unwrap();
-        let peer2_tx = ws_msg_sender.clone();
-
-        println!("Two clients paired! Starting WebRTC handshake...");
-
-        // Send each client the other's transmitter so they can forward messages
-        _ = peer1_peer_sender.send(Some(peer2_tx.clone()));
-        _ = peer_tx_sender.send(Some(peer1_tx.clone()));
-
-        // Tell first client to initiate the offer
-        let msg1 = serde_json::to_string(&MessageFromServer::PartnersReady { initiator: true })
-            .unwrap()
-            .into();
-        _ = peer1_tx.send(ws::Message::Text(msg1));
-
-        let msg2 = serde_json::to_string(&MessageFromServer::PartnersReady { initiator: false })
-            .unwrap()
-            .into();
-        _ = peer2_tx.send(ws::Message::Text(msg2));
-
-        // Close connections after clients establish P2P (give them 5 seconds for handshake)
-        let peer1_clone = peer1_tx.clone();
-        let peer2_clone = peer2_tx.clone();
         tokio::spawn(async move {
-            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-            println!("P2P connection established. Closing signaling connections...");
+            while let Some(Ok(ws_msg)) = from_client.next().await {
+                if let ws::Message::Close(_) = ws_msg {
+                    println!("Client connection closed");
+                    let mut clients = state.clients.lock().unwrap();
+                    if clients.remove(&client_id).is_none() {
+                        println!("[Should not happen] Client {client_id} disconnected, but was not in the clients map");
+                    }
+                    break;
+                }
 
-            let msg: Utf8Bytes = serde_json::to_string(&MessageFromServer::ServerClosing)
-                .unwrap()
-                .into();
-            _ = peer1_clone.send(ws::Message::Text(msg.clone()));
-            _ = peer2_clone.send(ws::Message::Text(msg));
+                let ws::Message::Text(text) = ws_msg else {
+                    println!("[Should not happen] Unknown message from client: {ws_msg:?}");
+                    let mut to_client = to_client.lock().unwrap();
+                    _ = to_client.send(MessageFromServer::BadRequest.ws_msg());
+                    continue;
+                };
 
-            // Close connections by sending Close message
-            _ = peer1_clone.send(ws::Message::Close(None));
-            _ = peer2_clone.send(ws::Message::Close(None));
+                let Ok(msg_from_client) = serde_json::from_str::<MessageFromClient>(&text) else {
+                    println!("[Should not happen] Error parsing client message");
+                    let mut to_client = to_client.lock().unwrap();
+                    _ = to_client.send(MessageFromServer::BadRequest.ws_msg());
+                    continue;
+                };
+
+                match msg_from_client {
+                    MessageFromClient::GetRooms => handle_get_rooms(),
+                    MessageFromClient::CreateRoom => handle_create_room(),
+                    MessageFromClient::JoinRoom { room_id } => handle_join_room(room_id),
+                }
+            }
         });
-    }
+    })
 }
+
+fn handle_get_rooms() {}
+
+fn handle_create_room() {}
+
+fn handle_join_room(room_id: u64) {}
+
+// (String, SocketAddr) -> u64
+
+// let room_id = (host_id, addr);
+
+// if rooms.contains_key(&room_id) {
+//     println!("Client {host_id} already is hosting a room");
+
+//     let msg = serde_json::to_string(&MessageFromServer::RoomAlreadyExists)
+//         .unwrap()
+//         .into();
+//     _ = to_client.send(ws::Message::Text(msg));
+// } else {
+//     println!("Creating new room with client {host_id} as the host");
+
+//     let msg = serde_json::to_string(&MessageFromServer::RoomCreated)
+//         .unwrap()
+//         .into();
+//     _ = to_client.send(ws::Message::Text(msg));
+
+//     let host = Client {
+//         id: host_id,
+//         addr,
+//         comm: to_client,
+//     };
+//     rooms.insert(
+//         room_id,
+//         Room {
+//             max_size: 4,
+//             host,
+//             guests: HashMap::new(),
+//         },
+//     );
+// }
